84d33c332714b108c727d891d534aa5b9b697eba
Marti Bolivar <marti.bolivar@linaro.org>
2017-02-27 15:59:18 -0500

    tcp.h: add missing include guards
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>

--- src/tcp.h
+++ src/tcp.h
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef __FOTA_TCP_H__
+#define __FOTA_TCP_H__
+
 /*
  * TCP Buffer Logic:
  * HTTP Header (17) + 3x MTU Packets 640 = 1937
@@ -16,3 +19,5 @@ int tcp_send(const unsigned char *buf, size_t size);
 int tcp_recv(unsigned char *buf, size_t size, int32_t timeout);
 struct net_context *tcp_get_context(void);
 struct k_sem *tcp_get_recv_wait_sem(void);
+
+#endif	/* __FOTA_TCP_H__ */
