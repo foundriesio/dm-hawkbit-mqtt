a572aa53a2fbab182416e036ada84f33d282ec4f
Marti Bolivar <marti.bolivar@linaro.org>
2017-04-20 20:42:57 -0400

    device: just expose a product ID
    Move the SOC-specific registers into device.c. They have no users, and
    nobody needs to know how the sausage is made, anyway.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>

--- src/device.h
+++ src/device.h
@@ -7,25 +7,6 @@
 #ifndef __FOTA_DEVICE_H__
 #define __FOTA_DEVICE_H__
 
-#include <board.h>
-
-/*
- * General hardware specific configs
- *
- * DEVICE_ID_BASE  : beginning of HW UID registers
- * DEVICE_ID_LENGTH: length of HW UID registers in 32-bit words
- */
-#if defined(CONFIG_SOC_SERIES_NRF52X)
-#define DEVICE_ID_BASE		(&NRF_FICR->DEVICEID[0])
-#define DEVICE_ID_LENGTH	2
-#elif defined(CONFIG_SOC_SERIES_STM32F4X)
-#define DEVICE_ID_BASE		UID_BASE
-#define DEVICE_ID_LENGTH	3
-#elif defined(CONFIG_SOC_SERIES_KINETIS_K6X)
-#define DEVICE_ID_BASE		(&SIM->UIDH)
-#define DEVICE_ID_LENGTH	4
-#endif
-
 struct product_id_t {
 	const char *name;
 	uint32_t number;
