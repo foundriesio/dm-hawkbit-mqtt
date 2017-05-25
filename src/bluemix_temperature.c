03916ad8816476a8dffdeba042925af8e73db5cc
Marti Bolivar <marti.bolivar@linaro.org>
2017-05-25 10:34:02 -0400

    Add app-level stubs for Bluemix temperature sensor
    This is prep work for extracting the temperature sensing back out of
    the Bluemix support core.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>

---
+++ src/bluemix_temperature.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+
+#include "bluemix_temperature.h"
+
+int bluemix_temperature_start(void)
+{
+	/* TODO: stub */
+	return -ENOTSUP;
+}
