837315e4bebc2494e03822e8a0041d1b50c347c4
Michael Scott <michael.scott@linaro.org>
2017-06-16 16:17:43 -0700

    ipss: follow upstream API changes for GATT services
    Upstream removed many of the duplicated attributes that every GATT related
    sample / app was using and now adds them automaticly using a new GATT service.
    
    Let's follow those upstream changes and fix our build.
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

--- src/bt_ipss.c
+++ src/bt_ipss.c
@@ -25,67 +25,12 @@
 #define DEVICE_CONNECTED_NAME_LEN (sizeof(DEVICE_CONNECTED_NAME) - 1)
 #define UNKNOWN_APPEARANCE 0x0000
 
-#if !defined(CONFIG_BLUETOOTH_GATT_DYNAMIC_DB)
-static ssize_t read_name(struct bt_conn *conn, const struct bt_gatt_attr *attr,
-			 void *buf, u16_t len, u16_t offset)
-{
-	const char *name = attr->user_data;
-
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, name,
-				 strlen(name));
-}
-
-static ssize_t read_appearance(struct bt_conn *conn,
-			       const struct bt_gatt_attr *attr, void *buf,
-			       u16_t len, u16_t offset)
-{
-	u16_t appearance = sys_cpu_to_le16(UNKNOWN_APPEARANCE);
-
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, &appearance,
-				 sizeof(appearance));
-}
-
-static ssize_t read_model(struct bt_conn *conn, const struct bt_gatt_attr *attr,
-			  void *buf, u16_t len, u16_t offset)
-{
-	const char *value = attr->user_data;
-
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, value,
-				 strlen(value));
-}
-
-static ssize_t read_manuf(struct bt_conn *conn, const struct bt_gatt_attr *attr,
-			  void *buf, u16_t len, u16_t offset)
-{
-	const char *value = attr->user_data;
-
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, value,
-				 strlen(value));
-}
-#endif /* CONFIG_BLUETOOTH_GATT_DYNAMIC_DB */
-
 static struct bt_gatt_attr attrs[] = {
-#if !defined(CONFIG_BLUETOOTH_GATT_DYNAMIC_DB)
-	BT_GATT_PRIMARY_SERVICE(BT_UUID_GAP),
-	BT_GATT_CHARACTERISTIC(BT_UUID_GAP_DEVICE_NAME, BT_GATT_CHRC_READ),
-	BT_GATT_DESCRIPTOR(BT_UUID_GAP_DEVICE_NAME, BT_GATT_PERM_READ,
-			   read_name, NULL, DEVICE_NAME),
-	BT_GATT_CHARACTERISTIC(BT_UUID_GAP_APPEARANCE, BT_GATT_CHRC_READ),
-	BT_GATT_DESCRIPTOR(BT_UUID_GAP_APPEARANCE, BT_GATT_PERM_READ,
-			   read_appearance, NULL, NULL),
-	/* Device Information Service Declaration */
-	BT_GATT_PRIMARY_SERVICE(BT_UUID_DIS),
-	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_MODEL_NUMBER, BT_GATT_CHRC_READ),
-	BT_GATT_DESCRIPTOR(BT_UUID_DIS_MODEL_NUMBER, BT_GATT_PERM_READ,
-			   read_model, NULL, CONFIG_SOC),
-	BT_GATT_CHARACTERISTIC(BT_UUID_DIS_MANUFACTURER_NAME,
-			       BT_GATT_CHRC_READ),
-	BT_GATT_DESCRIPTOR(BT_UUID_DIS_MANUFACTURER_NAME, BT_GATT_PERM_READ,
-			   read_manuf, NULL, "Manufacturer"),
-#endif /* CONFIG_BLUETOOTH_GATT_DYNAMIC_DB */
 	/* IP Support Service Declaration */
 	BT_GATT_PRIMARY_SERVICE(BT_UUID_IPSS),
 };
+
+static struct bt_gatt_service ipss_svc = BT_GATT_SERVICE(attrs);
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -105,7 +50,7 @@ int ipss_init(struct bt_conn_cb *conn_callbacks)
 {
 	int ret;
 
-	ret = bt_gatt_register(attrs, ARRAY_SIZE(attrs));
+	ret = bt_gatt_service_register(&ipss_svc);
 	if (!ret) {
 		bt_conn_cb_register(conn_callbacks);
 	}
