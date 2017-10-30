aa2fdd75b0f8fbef22aa34f293123cc7fd189486
Marti Bolivar <marti@opensourcefoundries.com>
2017-10-30 16:41:18 -0400

    dm-hawkbit-mqtt: make flake8 and python 3 happy for scripts/hawkbit.py
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- scripts/hawkbit.py
+++ scripts/hawkbit.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import argparse
 import requests
 import json
@@ -5,25 +7,27 @@ import json
 __version__ = 1.0
 
 user = 'admin'
-password  = 'admin'
+password = 'admin'
 DS_URL_DEFAULT = 'http://hawkbit:8080/rest/v1/distributionsets'
 SM_URL_DEFAULT = 'http://hawkbit:8080/rest/v1/softwaremodules'
+
 
 def publish(provider, name, type, version, description, artifact,
             ds_url, sm_url):
     # Publish Software Module
-    headers = { 'Content-Type': 'application/json',
-                'Accept': 'application/json' }
-    sm = { 'requiredMigrationStep': False,
-           'vendor': provider,
-           'name': name,
-           'type': type,
-           'description': description,
-           'version': version }
-    response = requests.post(sm_url, data=json.dumps([sm]), auth=(user, password), headers=headers)
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json'}
+    sm = {'requiredMigrationStep': False,
+          'vendor': provider,
+          'name': name,
+          'type': type,
+          'description': description,
+          'version': version}
+    response = requests.post(sm_url, data=json.dumps([sm]),
+                             auth=(user, password), headers=headers)
     if response.status_code != 500:
-        print json.loads(response.content)
-        for item in  json.loads(response.content):
+        print(json.loads(response.content))
+        for item in json.loads(response.content):
             if 'id' in item:
                 id = item['id']
             if '_links' in item:
@@ -35,40 +39,42 @@ def publish(provider, name, type, version, description, artifact,
                     type_url = item['_links']['type']['href']
                 if 'metadata' in item['_links']:
                     metadata_url = item['_links']['metadata']['href']
-        print artifacts_url
-        print self_url
-        print type_url
-        print metadata_url
-        print id
+        print(artifacts_url)
+        print(self_url)
+        print(type_url)
+        print(metadata_url)
+        print(id)
     # Upload Artifact
-    headers = {'Accept': 'application/json' }
+    headers = {'Accept': 'application/json'}
     artifacts = {'file': open(artifact, 'rb')}
-    response = requests.post(artifacts_url, auth=(user, password), headers=headers, files=artifacts)
+    response = requests.post(artifacts_url, auth=(user, password),
+                             headers=headers, files=artifacts)
     if response.status_code != 500:
-        headers = { 'Content-Type': 'application/json',
-                    'Accept': 'application/json' }
-        ds = { 'requiredMigrationStep': False,
-               'vendor': provider,
-               'name': name,
-               'type': type,
-               'description': description,
-               'version': version,
-               'modules': [{'id': id}],
-               '_links': {'artifacts': artifacts_url,
-                          'self': self_url,
-                          'type': type_url,
-                          'metadata': metadata_url} }
-        response = requests.post(ds_url, data=json.dumps([ds]), auth=(user, password), headers=headers)
+        headers = {'Content-Type': 'application/json',
+                   'Accept': 'application/json'}
+        ds = {'requiredMigrationStep': False,
+              'vendor': provider,
+              'name': name,
+              'type': type,
+              'description': description,
+              'version': version,
+              'modules': [{'id': id}],
+              '_links': {'artifacts': artifacts_url,
+                         'self': self_url,
+                         'type': type_url,
+                         'metadata': metadata_url}}
+        response = requests.post(ds_url, data=json.dumps([ds]),
+                                 auth=(user, password), headers=headers)
         if response.status_code != 500:
-            print response.content
-
-
+            print(response.content)
 
 
 def main():
     description = 'Simple Hawkbit API Wrapper'
-    parser = argparse.ArgumentParser(version=__version__, description=description)
-    parser.add_argument('-p', '--provider', help='SW Module Provider', required=True)
+    parser = argparse.ArgumentParser(version=__version__,
+                                     description=description)
+    parser.add_argument('-p', '--provider', help='SW Module Provider',
+                        required=True)
     parser.add_argument('-n', '--name', help='Name', required=True)
     parser.add_argument('-t', '--type', help='Name', required=True)
     parser.add_argument('-sv', '--swversion', help='Version', required=True)
@@ -82,6 +88,7 @@ def main():
     publish(args.provider, args.name, args.type, args.swversion,
             args.description, args.file, args.distribution_sets,
             args.software_modules)
+
 
 if __name__ == '__main__':
     main()
