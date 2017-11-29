8ead934f7016e67a5ba2ca2e49a59e46ac5b67fe
Tyler Baker <tyler@opensourcefoundries.com>
2017-11-29 13:16:49 -0800

    dm-hawkbit-mqtt: use mgmt.foundries.io for hawkbit host
    It was decided to migrate away from using the gitci.com hostname.
    Instead we would like to use mgmt.foundries.io. The
    additional changes needed for this migration have already
    been merged into the gateway-containers repo.
    
    Signed-off-by: Tyler Baker <tyler@opensourcefoundries.com>

--- src/lib/hawkbit.h
+++ src/lib/hawkbit.h
@@ -7,7 +7,7 @@
 #ifndef __FOTA_HAWKBIT_H__
 #define __FOTA_HAWKBIT_H__
 
-#define HAWKBIT_HOST	"gitci.com:8080"
+#define HAWKBIT_HOST	"mgmt.foundries.io:8080"
 #define HAWKBIT_PORT	8080
 #define HAWKBIT_JSON_URL "/DEFAULT/controller/v1"
 
