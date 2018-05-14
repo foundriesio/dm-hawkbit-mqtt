a7fbfa20005540fcc04ad1aef75f5283a73a7d6d
Michael Scott <michael@opensourcefoundries.com>
2018-05-14 10:19:09 -0700

    bluetooth: update BT address due to API changes
    Zephyr commit https://github.com/zephyrproject-rtos/zephyr/commit/d22b7c9f2d771bf9a2a1b8e0378795d4c507f558
    ("Bluetooth: Remove bt_storage API") removed the current method that
    we were using to set the BT MAC address.
    
    In a later commit https://github.com/zephyrproject-rtos/zephyr/commit/5b01cb1a460775e4532834f33a36eaef376813fd
    ("Bluetooth: Introduce new bt_set_id_addr() API") a new function was
    added to replace the removed functionality.
    
    Let's migrate out samples to this new function.
    
    Signed-off-by: Michael Scott <michael@opensourcefoundries.com>

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -16,7 +16,6 @@
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
-#include <bluetooth/storage.h>
 #include <bluetooth/conn.h>
 #include "bluetooth.h"
 
@@ -34,36 +33,7 @@
 #define GPIO_DRV_BT	BT_GPIO_PORT
 #endif
 
-/* Any by default, can change depending on the hardware implementation */
-static bt_addr_le_t bt_addr;
-
-static ssize_t storage_read(const bt_addr_le_t *addr, u16_t key, void *data,
-			       size_t length)
-{
-	if (addr) {
-		return -ENOENT;
-	}
-
-	if (key == BT_STORAGE_ID_ADDR && length == sizeof(bt_addr)) {
-		bt_addr_le_copy(data, &bt_addr);
-		return sizeof(bt_addr);
-	}
-
-	return -EIO;
-}
-
-static ssize_t storage_write(const bt_addr_le_t *addr, u16_t key,
-				const void *data, size_t length)
-{
-	return -ENOSYS;
-}
-
-static ssize_t storage_clear(const bt_addr_le_t *addr)
-{
-	return -ENOSYS;
-}
-
-static void set_own_bt_addr(void)
+static void set_own_bt_addr(bt_addr_le_t *addr)
 {
 	int i;
 	u8_t tmp;
@@ -73,10 +43,10 @@ static void set_own_bt_addr(void)
 	 */
 	for (i = 0; i < 4; i++) {
 		tmp = (product_id_get()->number >> i * 8) & 0xff;
-		bt_addr.a.val[i] = tmp;
+		addr->a.val[i] = tmp;
 	}
 
-	bt_addr.a.val[4] = 0xe7;
+	addr->a.val[4] = 0xe7;
 /*
  * For CONFIG_NET_L2_BT_ZEP1656, the U/L bit of the BT MAC is toggled by Zephyr.
  * Later that MAC is used to create the 6LOWPAN IPv6 address.  To account for
@@ -84,29 +54,10 @@ static void set_own_bt_addr(void)
  * gateway configuration for bt0).
  */
 #if defined(CONFIG_NET_L2_BT_ZEP1656)
-	bt_addr.a.val[5] = 0xd6;
+	addr->a.val[5] = 0xd6;
 #else
-	bt_addr.a.val[5] = 0xd4;
+	addr->a.val[5] = 0xd4;
 #endif
-}
-
-int bt_storage_init(void)
-{
-	static const struct bt_storage storage = {
-		.read = storage_read,
-		.write = storage_write,
-		.clear = storage_clear,
-	};
-
-	bt_addr.type = BT_ADDR_LE_RANDOM;
-
-	set_own_bt_addr();
-
-	bt_storage_register(&storage);
-
-	SYS_LOG_DBG("Bluetooth storage driver registered");
-
-	return 0;
 }
 
 /* BT LE Connect/Disconnect callbacks */
@@ -145,11 +96,19 @@ static struct bt_conn_cb conn_callbacks = {
 
 static int network_init(struct device *dev)
 {
+	bt_addr_le_t bt_addr;
+	int ret = 0;
+
 	/* Storage used to provide a BT MAC based on the serial number */
 	TC_PRINT("Setting Bluetooth MAC\n");
-	bt_storage_init();
+
+	memset(&bt_addr, 0, sizeof(bt_addr_le_t));
+	bt_addr.type = BT_ADDR_LE_RANDOM;
+	set_own_bt_addr(&bt_addr);
+	ret = bt_set_id_addr(&bt_addr);
 	bt_conn_cb_register(&conn_callbacks);
-	return 0;
+
+	return ret;
 }
 
 /* last priority in the POST_KERNEL init levels */
