8c8c1e946b4ca8370390b5ff3d1efb8588f341d0
Michael Scott <mike@foundries.io>
2018-09-14 22:50:24 -0700

    scripts/hawkbit.py: add rollout arguments
    To make deploying easier, let's add the following 2 arguments:
    --rollouts: the Rollout ReST API URL (optional)
    --rollout-filter: a device name filter to trigger a rollout creation.
      (optional: without this a rollout will not be created)
    
    A typical run of hawkbit.py might look like this for BLENano2:
    python zephyr-fota-samples/dm-hawkbit-mqtt/scripts/hawkbit.py \
      -sv "1.00B" \
      -p "FIO" \
      -n "ZmP-BLEnano2" \
      -d "Signed FIO ZmP - BLEnano2 - FOTA" \
      -t os \
      -rf "nrf52_blenano2" \
      -f outdir/zephyr-fota-samples/dm-hawkbit-mqtt/nrf52_blenano2/app/zephyr/dm-hawkbit-mqtt-nrf52_blenano2-signed.bin
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- scripts/hawkbit.py
+++ scripts/hawkbit.py
@@ -5,6 +5,7 @@ import pprint
 import requests
 import json
 import sys
+import time
 
 __version__ = 1.0
 
@@ -12,10 +13,53 @@ user = 'admin'
 password = 'admin'
 DS_URL_DEFAULT = 'http://localhost:8080/rest/v1/distributionsets'
 SM_URL_DEFAULT = 'http://localhost:8080/rest/v1/softwaremodules'
+RO_URL_DEFAULT = 'http://localhost:8080/rest/v1/rollouts'
+
+ROLLOUT_START_DELAY = 5
+
+def start_rollout(version, ds_id, ro_url, rollout_filter):
+    headers = {'Content-Type': 'application/hal+json;charset=UTF-8',
+               'Accept': 'application/hal+json'}
+    ds = {'distributionSetId': ds_id,
+          'successCondition': {'condition': 'THRESHOLD', 'expression': 100},
+          'successAction': {'expression': '', 'action': 'NEXTGROUP'},
+          'targetFilterQuery': 'name==' + rollout_filter + '*',
+          'name': 'RO-' + version,
+          'description': 'Rollout for ' + version + ' to ' + rollout_filter,
+          'amountGroups': 1,
+          'errorAction': {'expression': '', 'action': 'PAUSE'},
+          'errorCondition': {'condition': 'THRESHOLD', 'expression': 80}
+    }
+    response = requests.post(ro_url, data=json.dumps(ds),
+                             auth=(user, password), headers=headers)
+
+    if response.status_code == 500:
+        return
+
+    response = response.json()
+
+    print('Got response from server when creating rollout:')
+    pprint.pprint(response)
+
+    start_url = response['_links']['start'].get('href')
+
+    if start_url is None:
+        print("Couldn't parse response", file=sys.stderr)
+        return
+
+    print('Starting rollout ...')
+    time.sleep(ROLLOUT_START_DELAY)
+    response = requests.post(start_url, auth=(user, password))
+
+    if response.status_code == 500:
+        return
+
+    print('Started.')
 
 
 def publish_ds(provider, name, type, version, description, artifact,
-               ds_url, id, artifacts_url, self_url, type_url, metadata_url):
+               ds_url, ro_url, rollout_filter, sm_id, artifacts_url, self_url,
+               type_url, metadata_url):
     # Upload Artifact
     headers = {'Accept': 'application/json'}
     with open(artifact, 'rb') as f:
@@ -33,20 +77,38 @@ def publish_ds(provider, name, type, version, description, artifact,
           'type': type,
           'description': description,
           'version': version,
-          'modules': [{'id': id}],
+          'modules': [{'id': sm_id}],
           '_links': {'artifacts': artifacts_url,
                      'self': self_url,
                      'type': type_url,
                      'metadata': metadata_url}}
     response = requests.post(ds_url, data=json.dumps([ds]),
                              auth=(user, password), headers=headers)
-    if response.status_code != 500:
-        print('Got response from server when posting artifacts:')
-        pprint.pprint(response.json())
 
+    if response.status_code == 500:
+        return
+
+    response = response.json()
+
+    print('Got response from server when posting artifacts:')
+    pprint.pprint(response)
+
+    if rollout_filter is None:
+        return
+
+    ds_id = 0
+
+    for item in response:
+        ds_id = int(item.get('id', 0))
+
+    if ds_id == 0:
+        print("Couldn't parse artifact post response", file=sys.stderr)
+        return
+
+    start_rollout(version, ds_id, ro_url, rollout_filter)
 
 def read_sm(provider, name, type, version, description, artifact,
-            ds_url, id, self_url):
+            ds_url, ro_url, rollout_filter, id, self_url):
     # Read back detailed softwaremodule info
     headers = {'Accept': 'application/json'}
     response = requests.get(self_url,
@@ -73,11 +135,12 @@ def read_sm(provider, name, type, version, description, artifact,
         return
 
     publish_ds(provider, name, type, version, description, artifact,
-               ds_url, id, artifacts_url, self_url, type_url, metadata_url)
+               ds_url, ro_url, rollout_filter, id, artifacts_url, self_url,
+               type_url, metadata_url)
 
 
 def publish_sm(provider, name, type, version, description, artifact,
-               ds_url, sm_url):
+               ds_url, sm_url, ro_url, rollout_filter):
     # Publish Software Module
     headers = {'Content-Type': 'application/json',
                'Accept': 'application/json'}
@@ -114,7 +177,7 @@ def publish_sm(provider, name, type, version, description, artifact,
         return
 
     read_sm(provider, name, type, version, description, artifact,
-            ds_url, id, self_url)
+            ds_url, ro_url, rollout_filter, id, self_url)
 
 
 def main():
@@ -132,10 +195,14 @@ def main():
                         help='Distribution Sets URL', default=DS_URL_DEFAULT)
     parser.add_argument('-sm', '--software-modules',
                         help='Software Modules URL', default=SM_URL_DEFAULT)
+    parser.add_argument('-ro', '--rollouts',
+                        help='Rollouts URL', default=RO_URL_DEFAULT)
+    parser.add_argument('-rf', '--rollout-filter', help='Rollout name filter',
+                        default=None)
     args = parser.parse_args()
     publish_sm(args.provider, args.name, args.type, args.swversion,
                args.description, args.file, args.distribution_sets,
-               args.software_modules)
+               args.software_modules, args.rollouts, args.rollout_filter)
 
 
 if __name__ == '__main__':
